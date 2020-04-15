7e70b45d72e966617e9ce52784a1e40fff2dc010
hiroki takei <takei0802@icloud.com>
2020-04-16 00:24:00 +0900

    Create index.js

---
+++ .vscode/index.js
@@ -0,0 +1,18 @@
+let youtuber = {
+    list: { 
+        business: {
+            youtuber: [
+                { name: "しまぶー", age: 20, teachProgramming() {} },
+                { name: "あっちゃん", age: 37, teachHistory() {} }
+            ],
+            teach() {}
+        },
+        entertaiment: {
+            youtuber: [{ name: "ヒカキン"}, { name: "はじめしゃちょー"}],
+            makeSmile() {}
+        }    
+    },
+    plan() {},
+    uploadVideo() {}
+};
+
